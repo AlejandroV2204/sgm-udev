53ef193c6aeb022a84a2eec333554407a16a6844
AlejandroV2204 <jvallarinocruz@gmail.com>
2022-02-23 16:56:35 -0500

    Arreglando errores.

---
+++ reporte/crearReporte.controller.php
@@ -0,0 +1,29 @@
+<?php 
+
+include_once('../header.php');
+include_once('../conexion.php');
+
+$id_usuario1 = @$_POST['id_usuario1'];
+$id_PC1 = @$_POST['id_PC1'];
+$descripcion = @$_POST['descripcion'];
+
+echo "<br>"  . $id_usuario1 . "<br>" . $id_PC1 . "<br>" . $descripcion .  "<br><br>" ;
+
+$sql = "INSERT INTO usuario VALUES ('default', '$id_usuario1', '$id_PC1', '$descripcion')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+header("location:reporte.list.php");
+
+function guardar () {}
+
+?>
+
+<?php 
+include_once('../footer.php');
+?>
+
