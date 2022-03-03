ba2ad009309950878868347345853f1c8ee891de
d1lanX <rodriguezdylan58@gmail.com>
2022-03-03 15:06:40 -0500

    cambio de base de datos

--- conexion.php
+++ conexion.php
@@ -1,4 +1,6 @@
 <?php
-$link = mysqli_connect("db4free.net", "udev_edu", "OscarJavier");
-mysqli_select_db($link, "sgmudev");
+
+$link = mysqli_connect("bs8xt34tfk2kcawkul5c-mysql.services.clever-cloud.com", "ukr70bves9og4fn5", "tsQBVGEuldvU2dl3D6x7");
+
+mysqli_select_db($link, "bs8xt34tfk2kcawkul5c");
 $tildes = $link->query("SET NAMES 'utf8'"); //Para que se muestren las tildes correctamente
