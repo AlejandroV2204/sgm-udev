5a97e9aa2cd5e6f21ec10248148b35918b96abd3
JulianEstebanRamirezCordoba <julianestebanramirezcordoba25@gmail.com>
2022-03-11 15:24:30 -0500

    Se prueva todo y esta bien

--- util/utilModelo.php
+++ util/utilModelo.php
@@ -57,10 +57,11 @@
             for ($i = 0; $i < count($valores); $i++) {
                  $construccionDeValores = ($i == (count($valores) - 1)) ? $construccionDeValores . "`" . $campos[$i] . "` = '" . $valores[$i] . "'" : $construccionDeValores . "`" . $campos[$i] . "` = '" . $valores[$i] . "',";
             }
+
              $consulta = "UPDATE `$tabla` SET $construccionDeValores WHERE `$campoCondicion` = '$condicion' ;";
              $query = mysqli_query($link, $consulta);
              echo $consulta; 
-                die();
+                // die();
            return $query;
         }
 
