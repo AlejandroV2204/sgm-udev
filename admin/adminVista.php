8a037c31aa7254c67e1565438c6b640c08874a69
d1lanX <rodriguezdylan58@gmail.com>
2022-03-22 16:56:42 -0500

    el tamanio del logo ahora deberia ser normal

--- admin/adminVista.php
+++ admin/adminVista.php
@@ -21,7 +21,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>NOMBRE S.A.</title>
+    <title>u_dev</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <link href="../css/bootstrap.min.css" rel="stylesheet">
