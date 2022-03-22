2e418cf0bb8e3268a33a7813a9db462e1c3fd047
d1lanX <rodriguezdylan58@gmail.com>
2022-03-22 17:22:01 -0500

    cambio en el lgo

--- componentes/menuPrincipalAdmin.php
+++ componentes/menuPrincipalAdmin.php
@@ -22,7 +22,7 @@
     <div class="navbar-inner">
         <div class="container"><a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
                         class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a
-                    class="brand" href="">SGM_UDEV</a>
+                    class="brand" href=""><img src="../img/udev_logo.png"></a>
             <div class="nav-collapse">
                 <ul class="nav pull-right">
 
