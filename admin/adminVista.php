e30dfed2c3c4a6b92bb702785312ffb867d10456
d1lanX <rodriguezdylan58@gmail.com>
2022-03-23 13:50:02 -0500

    archivos prueba

--- admin/adminVista.php
+++ admin/adminVista.php
@@ -33,16 +33,17 @@
     <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-</head>
-
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/chart.min.js" type="text/javascript"></script>
+  </head>
 <body>
     <?php
     include "../componentes/menuPrincipalAdmin.php";
-?>
+    ?>
     <div class="main">
-        <div class="main-inner">
-            <div class="container">
-                <div class="row">
+      <div class="main-inner">
+        <div class="container">
+          <div class="row">
 
                     <div id="chart-container">
                         <canvas id="graphCanvas"></canvas>
@@ -55,7 +56,7 @@
 
                     function showGraph() {
                         {
-                            $.post("verControlVista.php",
+                            $.post("data.php",
                                 function(data) {
                                     console.log(data);
                                     var name = [];
@@ -96,9 +97,7 @@
                 <!-- Le javascript
 <-- ============================================================================================ -->
                 <!-- Placed at the end of the document so the pages load faster -->
-                <script src="../js/jquery.min.js"></script>
                 <script src="../js/excanvas.min.js"></script>
-                <script src="../js/chart.min.js" type="text/javascript"></script>
                 <script src="../js/bootstrap.js"></script>
                 <script language="javascript" type="text/javascript" src="../js/full-calendar/fullcalendar.min.js">
                 </script>
