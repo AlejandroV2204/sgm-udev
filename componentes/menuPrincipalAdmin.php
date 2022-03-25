56f1f44ca8fd20b2933f26fb53513ec2b2f214ef
JulianEstebanRamirezCordoba <julianestebanramirezcordoba25@gmail.com>
2022-03-24 19:28:42 -0500

    Se suben datos

--- componentes/menuPrincipalAdmin.php
+++ componentes/menuPrincipalAdmin.php
@@ -16,49 +16,24 @@
         }
 
     }
-    
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Usuarios</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <link href="../css/bootstrap.min.css" rel="stylesheet">
-  <link href="../css/bootstrap-responsive.min.css" rel="stylesheet">
-  <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
-  rel="stylesheet">
-  <link href="../css/font-awesome.css" rel="stylesheet">
-  <link href="../css/style.css" rel="stylesheet">
-  <link href="../css/pages/dashboard.css" rel="stylesheet">
-  <link href="../css/pages/plans.css" rel="stylesheet">
-  <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-  <!--[if lt IE 9]>
-  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
-</head>
-<body>
 <div class="navbar navbar-fixed-top">
     <div class="navbar-inner">
         <div class="container"><a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
-                    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a>
-                    <a class="brand" ><img src="../img/udev_logo2.png"></a>
+                        class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a>
+          <a class="brand" ><img src="../img/udev_logo2.png"></a>
             <div class="nav-collapse">
                 <ul class="nav pull-right">
 
-                    <h2>
-
-                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
+                    <h2><li class="dropdown"><a href="" class="dropdown-toggle" data-toggle="dropdown"><i
                                     class="icon-user"></i>
+                                    <?php echo $nombre; ?>
+                                    <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
 
-
-                                <?php echo $nombre; ?>
-                                <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-
-
-                                <!-- Validar opciones para mi admin -->
+                        
+                               <!-- Validar opciones para mi admin -->
                                 <?php
                                 $util->validarElemento(0, '<li><a href="../admin/verControlVista.php"><i class="icon-bell"></i>Control Tecnico</a></li>');
                                 $util->validarElemento(0, '<li><a href="../cambiarPassword/cambiarPasswordVista.php"><i class="icon-key"></i>Cambiar contraseña</a></li>');
@@ -71,13 +46,13 @@
                                 $util->validarElemento(3, '<li><a href="../cambiarPassword/cambiarPasswordVista.php"><i class="icon-key"></i>Cambiar contraseña</a></li>');
                             ?>
 
+                            
 
-
-                                <li><a href="../seguridad/cerrarSesion.php"><i class="icon-signout"></i>Cerrar sesion</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </h2>
+                            <li><a href="../seguridad/cerrarSesion.php"><i class="icon-signout"></i>Cerrar sesion</a>
+                            </li>
+                        </ul>
+                    </li>
+                </h2>
                 </ul>
             </div>
             <!--/.nav-collapse -->
@@ -129,6 +104,3 @@
     <!-- /subnavbar-inner -->
 </div>
 <!-- /subnavbar -->
-    
-</body>
-</html>
