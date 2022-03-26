df356c1aad5585dfefce7829901baadc2801e8e1
JulianEstebanRamirezCordoba <julianestebanramirezcordoba25@gmail.com>
2022-03-25 23:37:02 -0500

    Se suben datos

--- componentes/pie.php
+++ componentes/pie.php
@@ -1,17 +1,50 @@
-
-
-<!-- /extra -->
-<div class="footer">
-    <div class="footer-inner">
-        <div class="container">
-            <div class="row">
+<!-- /main -->
+ <!-- <div class="extra">
+  <div class="extra-inner">
+    <div class="container">
+      <div class="row">
+        <div class="span3">
+          <h4>
+            About Free Admin Template</h4>
+            <ul>
+              <li><a href="javascript:;">EGrappler.com</a></li>
+              <li><a href="javascript:;">Web Development Resources</a></li>
+              <li><a href="javascript:;">Responsive HTML5 Portfolio Templates</a></li>
+              <li><a href="javascript:;">Free Resources and Scripts</a></li>
+            </ul>
+          </div>
+       
+          <div class="span3">
+            <h4>
+              Support</h4>
+              <ul>
+                <li><a href="javascript:;">Frequently Asked Questions</a></li>
+                <li><a href="javascript:;">Ask a Question</a></li>
+                <li><a href="javascript:;">Video Tutorial</a></li>
+                <li><a href="javascript:;">Feedback</a></li>
+              </ul>
             </div>
-
+            
+          </div>
+      
         </div>
-        <!-- /row -->
+      
+      </div>
+   
+    </div> -->
+
+ 
+    <div class="footer">
+      <div class="footer-inner">
+        <div class="container">
+          <div class="row">
+            <div class="span12"></div>  
+           
+          </div>
+         
+        </div>
+        
+      </div>
+   
     </div>
-    <!-- /container -->
-</div>
-<!-- /footer-inner -->
-</div>
-<!-- /footer -->
+  
